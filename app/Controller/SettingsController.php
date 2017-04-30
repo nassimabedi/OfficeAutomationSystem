7cbd152ed0d0c96905dc041f3729f1af0a5becfd
Nassim Abedi <nassimabedi@gmail.com>
2017-04-30 10:39:23 -0700

    adding Console,Controller

---
+++ app/Controller/SettingsController.php
@@ -0,0 +1,18 @@
+<?php
+
+App::uses('AppController', 'Controller');
+class SettingsController extends AppController {
+        public $scaffold;
+        public $helpers = array('Html', 'Form','Js');
+        var $uses = array('Settings');
+
+
+        function index() {
+
+        }
+
+
+}
+
+
+?>
