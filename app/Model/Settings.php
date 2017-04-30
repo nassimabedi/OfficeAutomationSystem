4d54633ac87771e1dbb5aac2b56ba43121e342e4
Nassim Abedi <nassimabedi@gmail.com>
2017-04-30 10:45:41 -0700

    lib,model

---
+++ app/Model/Settings.php
@@ -0,0 +1,9 @@
+<?php
+//App::uses('AppModel', 'Model');
+class Settings extends AppModel {
+        var $useTable = 'settings';
+
+}
+
+
+?>
---
+++ app/Model/ShiftServices.php
@@ -0,0 +1,6 @@
+<?php
+class ShiftServices extends AppModel {
+        var $useTable = 'shift_services';
+
+}
+?>
